24f8f9a5c712164c189b6342127991245ecac8ab
taschunina <taschunina@yandex.ru>
2020-08-29 12:44:08 +0300

    Добавлен логин в роут юзерс

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const {
-  getUsers, getUserById, createUser
+  getUsers, getUserById, login, createUser
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
+router.post('/signin', login);
 router.post('/', createUser);
 
 module.exports = router;
