fb1ecded04dfdfb0bc1bd306106e14e015450dfb
taschunina <taschunina@yandex.ru>
2020-09-04 15:54:22 +0300

    Мидлвара авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,22 @@
-req.user = payload;
-next();
-
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    console.log(req.headers);
+    return res.status(401).send({ message: 'Необходима авторизация_Не авторизован' });
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, '12345678');
+    console.log('payload');
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    console.log('err');
+    return res.status(401).send({ message: 'Необходима авторизация2' });
   }
 
   req.user = payload;
