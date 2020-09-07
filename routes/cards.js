b9cc983d05a83a134843ccbea4455f28dc5f51b6
taschunina <taschunina@yandex.ru>
2020-09-07 22:48:15 +0300

    Настройка валидации

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
+const { celebrate, Joi} = require('celebrate');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const {
   getCards, createCard, deleteCardById
 } = require('../controllers/cards');
 
 router.get('/', auth, getCards);
-router.post('/', auth, createCard);
+router.post('/', auth, celebrate ({body: Joi.object().keys({
+  name: Joi.string().required().min(2).max(30),
+  link: Joi.string().required().link(),
+  }) , createCard);
 router.delete('/:id', auth, deleteCardById);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
+const { celebrate, Joi} = require('celebrate');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const {
   getUsers,
@@ -11,6 +13,12 @@ const {
 router.get('/', auth, getUsers);
 router.get('/:userId', auth, getUserById);
 router.post('/signin', login);
-router.post('/signup', createUser);
+router.post('/signup', celebrate ({body: Joi.object().keys({
+  name: Joi.string().required().min(2).max(30),
+  about: Joi.string().required().min(2).max(30),
+  avatar: Joi.string().required(),
+  email: Joi.string().required().email(),
+  password: Joi.string().required().min(8),
+  }) , createUser);
 
 module.exports = router;
