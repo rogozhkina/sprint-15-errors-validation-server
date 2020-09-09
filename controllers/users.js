7b53ae01a978d95e591404af54fdd191c0e30d76
taschunina <taschunina@yandex.ru>
2020-09-09 23:47:31 +0300

    Устранена ошибка зависания при попытке получения валидного юзера

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const ValidationError = require('../errors/validation-err');
+const NotFoundError = require('../errors/not-found-err');
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
@@ -12,8 +13,12 @@ module.exports.getUsers = (req, res, next) => {
 module.exports.getUserById = (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => {
+      console.log(user);
       if (user != null) {
+        console.log(user);
         res.send({ data: user });
+      } else if (user === null) {
+        throw new NotFoundError('Запрашиваемый ресурс не найден');
       }
     })
     .catch(next);
